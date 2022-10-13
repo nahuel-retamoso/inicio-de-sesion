cf52dc2c51bd5bc3cb64b2c503e012370b1a05c7
tumijiu <nahuel.retamoso@gmail.com>
2022-10-13 00:38:12 -0300

    commit

--- index.js
+++ index.js
@@ -86,3 +86,101 @@ app.post('/login', (req, res) => {
     }
     res.redirect('/api/products-tests');
 });
+
+
+const { create } = require('connect-mongo');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('./models');
+
+
+passport.use('signup', new LocalStrategy({
+    passReqToCallback: true
+}, (req, username, password, done) => {
+    User.findOne({ username }, (err, user) => {
+        if (err) {
+            return done(err);
+        }
+
+        if (user) {
+            return done(null, false, { message: 'El usuario ya existe' });
+        }
+
+        const newUser = new User({
+            username: username,
+            password: createHash(password),
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName
+        });
+
+        newUser.save((err) => {
+            if (err) {
+                return done(err);
+            }
+
+            return done(null, newUser);
+        });
+    });
+}  
+));
+
+const createHash = (password) => {
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+}
+
+passport.use('login', new LocalStrategy({
+    passReqToCallback: true
+}, (req, username, password, done) => {
+    User.findOne({ username }, (err, user) => {
+        if (err) {
+            return done(err);
+        }
+
+        if (!user) {
+            return done(null, false, { message: 'El usuario no existe' });
+        }
+
+        if (!isValidPassword(user, password)) {
+            return done(null, false, { message: 'Contraseña incorrecta' });
+        }
+
+        return done(null, user);
+    });
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
+
+const isValidPassword = (user, password) => {
+    return bCrypt.compareSync(password, user.password);
+}
+
+
+app.get('/signup', (req, res) => {
+    res.render('signup.hbs');
+});
+
+app.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/private',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+
+app.get('/login', (req, res) => {
+    res.render('login.hbs');
+});
+
+app.post('/login', passport.authenticate('login', {
+    successRedirect: '/api/products-tests',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
